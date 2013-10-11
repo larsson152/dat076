d7cb297a20a8cd44661615ca109926d66a171c5d
Patrik Larsson <patriklarsson152@gmail.com>
2013-10-11 14:37:21 +0200

    Added user to project

---
+++ Blogbox/src/main/java/com/dat076/blogbox/core/UserRegistry.java
@@ -0,0 +1,30 @@
+package com.dat076.blogbox.core;
+
+import com.dat076.blogbox.utils.AbstractDAO;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Patrik Larsson
+ */
+public final class UserRegistry extends AbstractDAO<User, Long> implements IUserRegistry {
+
+   
+    
+    public UserRegistry(String puName) {
+        super(User.class, puName);
+       
+    }
+    
+    @Override
+    public List<User> getByName(String name) {
+        List<User> found = new ArrayList<>();
+        for (User u : getRange(0, getCount())) {
+            if (u.getName().equals(name)) {
+                found.add(u);
+            }
+        }
+        return found;
+    }
+}
