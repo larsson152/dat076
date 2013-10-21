66e186f6a440f386a4b0704feb5dce99393b9773
Patrik Larsson <patriklarsson152@gmail.com>
2013-10-21 21:17:19 +0200

    fixed date on clock and blogposts and some small changes

--- BlogBoxMaven/src/main/webapp/js/clock.js
+++ BlogBoxMaven/src/main/webapp/js/clock.js
@@ -1,34 +1,38 @@
+//Gets the current Date object extracts hours minutes and seconds and sets the
+// p tag in the xhtml
 function startTime()
 {
-var currentDate=new Date();
+    var currentDate = new Date();
 
-var h=currentDate.getHours();
-var m=currentDate.getMinutes();
-var s=currentDate.getSeconds();
+    var h = currentDate.getHours();
+    var m = currentDate.getMinutes();
+    var s = currentDate.getSeconds();
 
-m=checkTime(m);
-s=checkTime(s);
-document.getElementById('clock').innerHTML=h+":"+m+":"+s;
-t=setTimeout(function(){startTime();},500);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function() {
+        startTime();
+    }, 500);
 }
-
+//Adds a zero(0) infront of a single number to maek it two digit
 function checkTime(i)
 {
-if (i<10)
-  {
-  i="0" + i;
-  }
-return i;
+    if (i < 10)
+    {
+        i = "0" + i;
+    }
+    return i;
 }
-
+//Gets the current date object and extracts the day of month, month and year and
+//sets the p tag in the xhtml.
 function showDate()
 {
-    var currentDate=new Date();
-    
-    var day=currentDate.getDay();
-    var month=currentDate.getMonth();
-    var year=currentDate.getFullYear();
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth();
+    month = month+1;
+    var year = currentDate.getFullYear();
     clearTimeout(t);
-    document.getElementById('clock').innerHTML=day+"/"+month+" "+year;
-    
+    document.getElementById('clock').innerHTML = day + "/" + month + " " + year;
 }
