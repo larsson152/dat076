cf57c043776df44516e19bf55643abf6b93f3f13
Patrik Larsson <patriklarsson152@gmail.com>
2013-10-23 21:27:28 +0200

    removed some unused imports

--- BlogBoxMaven/src/main/java/com/dat076/blogbox/entities/Blog.java
+++ BlogBoxMaven/src/main/java/com/dat076/blogbox/entities/Blog.java
@@ -5,13 +5,10 @@
 package com.dat076.blogbox.entities;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 /**
